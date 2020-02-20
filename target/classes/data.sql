08e9cec47a9fedc524f231192c3a50d667cbd95c
Ganesh Karthick Kandasamy <M1045990@mindtree.com>
2020-02-20 19:51:18 +0530

    Initial Commit

---
+++ target/classes/data.sql
@@ -0,0 +1,16 @@
+DELETE FROM users;
+DELETE FROM color;
+
+INSERT INTO color values ('1','RED');
+INSERT INTO color values ('2','ORANGE');
+INSERT INTO color values ('3','BLUE');
+INSERT INTO color values ('4','GREEN');
+INSERT INTO color values ('5','YELLOW');
+
+
+INSERT INTO users values ('ganesh','password111','ganesh.kandasamy@mindtree.com');
+INSERT INTO users values ('suresh','password221','ganesh.kandasamy@mindtree.com');
+INSERT INTO users values ('ramesh','password3311','ganesh.kandasamy@mindtree.com');
+INSERT INTO users values ('satheesh','password411','ganesh.kandasamy@mindtree.com');
+INSERT INTO users values ('pavan','password151','ganesh.kandasamy@mindtree.com');
+INSERT INTO users values ('basava','password161','ganesh.kandasamy@mindtree.com');
